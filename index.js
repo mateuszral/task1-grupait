15919c568a616e6c1ca4d4e274e50ddce76b29da
mateuszral <mateuszral7@gmail.com>
2021-12-02 12:33:46 +0100

    finished

---
+++ index.js
@@ -0,0 +1,22 @@
+const generateButton = document.querySelector('.generateButton');
+const oddNumbersDiv = document.querySelector('.oddNumbers');
+const evenNumbersDiv = document.querySelector('.evenNumbers');
+
+generateButton.addEventListener('click', () => {
+  const randomNumbers = [];
+  for (let i = 0; i < 20; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 100) + 1);
+  }
+  randomNumbers.sort((a, b) => a - b);
+
+  randomNumbers.forEach((number) => {
+    const numberElement = document.createElement('p');
+    numberElement.innerText = number;
+
+    if (number % 2 === 0) {
+      evenNumbersDiv.appendChild(numberElement);
+    } else {
+      oddNumbersDiv.appendChild(numberElement);
+    }
+  });
+});
